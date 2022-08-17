e06d31c840b0a1c82ef6ca302bd1c0dc593215ac
educavalieri <eduardocavalieri@hotmail.com>
2022-08-17 19:17:19 -0300

    project implemented

---
+++ src/main/java/com/educavalieri/MicroserviceTests/MicroserviceTestsApplication.java
@@ -0,0 +1,13 @@
+package com.educavalieri.MicroserviceTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTestsApplication.class, args);
+	}
+
+}
